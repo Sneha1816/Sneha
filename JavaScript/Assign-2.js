8a1e32d465544d1eb4434f45c4b952caa19427c0
Sneha1816 <snehashinde1816@gmail.com>
2022-04-29 16:12:27 +0530

    Task 2 Done

---
+++ JavaScript/Assign-2.js
@@ -0,0 +1,9 @@
+
+ let Emp_Info = {username:"sneha", password:123, emailid:"snehashinde1816@gmail.com", adreess:"pune", salary:25000};
+  
+ let Information = "username" + " : " +  Emp_Info.username + " " + "password " + " :"  +  Emp_Info.password + " " + "email-id" + " : " + Emp_Info.emailid + " " + "address" + " : " + Emp_Info.adreess + " " + "salary" + " : " + Emp_Info.salary ;
+ console.log(Information);
+
+ console.log(Emp_Info);
+ let Emp_Info1=  "username" + " : " + "sneha" ;
+console.log(Emp_Info1);
