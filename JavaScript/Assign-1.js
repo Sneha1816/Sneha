ba0b59ec8979fe2af816350d2a292434831b82b6
Sneha1816 <snehashinde1816@gmail.com>
2022-04-29 16:09:55 +0530

    Task 1 Done

---
+++ JavaScript/Assign-1.js
@@ -0,0 +1,25 @@
+let username ="sneha";
+let age = 24;
+let another_name ="pratik";
+let another_age = 25;
+
+let a = username;
+let b = age;
+let c = another_name;
+let d = another_age;
+
+
+
+username = c;
+console.log(username);
+
+
+age = d;
+console.log(age);
+
+
+another_name = a;
+console.log(another_name);
+
+another_age = b;
+console.log(another_age);
